c256fbcb4922f68a74a0ca5d9dffa3f9fd21aaf7
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-03-14 15:04:23 -0400

    cleaned up task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,49 +9,11 @@ class TasksController < ApplicationController
       render json: task.to_json(task_serializer_options)
    end
 
-   # DOES NOT WORK 
-   # def create
-   #    task = Task.new(task_params)
-   #    if task.save
-   #       render json: task.to_json(task_serializer_options)
-   #    else
-   #       render json: {error: "Unable to create new task"}
-   #    end
-   # end
 
-   # VERSION # 1 - WORKS EXCEPT CAN'T READ 
-   # def create
-   #    task = Task.create(task_serializer_options)
-   #    render json: task
-   # end
-
-   # VERSION # 3  - WORKS AND I SEE DATA IN RAILS CONSOLE, BUT DOES NOT PERSIST
-   # def create
-   #    task = Task.create(task_params)
-   #    render json: task
-   # end
-
-   # DOES NOT PERSIST RIGHT AWAY 
-   # MISSING TITLE AT UPDATE 
    def create
       task = Task.create(task_params)
       render json: task.to_json(task_serializer_options)
    end
-
-
-   # def create
-   #    task = Task.new(task_params)
-   #    if task.save
-   #       render json: task.to_json(
-   #          :include => {
-   #             :customer => {:only => [:first_name, :last_name, :company, :id]}, 
-   #             :user => {:only => [:first_name, :last_name, :id]}
-   #          }, :except => [:updated_at]
-   #       )
-   #    end
-   # end
-
-
 
    # THIS WORKS!!!!! 
    def update
@@ -70,7 +32,6 @@ class TasksController < ApplicationController
 
    def task_params
       params.require(:task).permit(:title, :task_type, :description, :due_date, :time_due, :notes, :user_id, :customer_id)
-      # params.require(:task).permit!
    end
 
  
